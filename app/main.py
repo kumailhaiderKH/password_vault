466b54586e5f19e282987e04ebdb07d2583cb6ed
Kumail <kumailhaider.kh@gmail.com>
2026-05-09 21:02:40 +0530

    second commit

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!!!"}
 
 app.include_router(user.router)
 app.include_router(auth.router)
