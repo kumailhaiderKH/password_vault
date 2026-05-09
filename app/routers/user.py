f12b3ad609b05ce25d5b44dca0c9cefdd3e088cc
Kumail <kumailhaider.kh@gmail.com>
2026-05-09 22:48:14 +0530

    response model changes

--- app/routers/user.py
+++ app/routers/user.py
@@ -7,7 +7,7 @@ router = APIRouter(
     prefix= '/users'
 )
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def createuser(user: schemas.UserCreate, db:Session = Depends(get_db)):
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
@@ -17,7 +17,7 @@ def createuser(user: schemas.UserCreate, db:Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@router.get("/")
+@router.get("/", response_model=list[schemas.UserOut])
 def get_users(db:Session = Depends(get_db)):
     users = db.query(models.User).all()
     return users
--- app/schemas.py
+++ app/schemas.py
@@ -7,13 +7,32 @@ from datetime import datetime
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
+    class Config:
+        from_attributes = True
+
+class UserOut(BaseModel):
+    id : int
+    email: EmailStr
+    created_at: datetime
+    class Config:
+        from_attributes = True
 
 class add_password(BaseModel):
     platform: str
     website_URL: str
     platform_username: str
     platform_password: str
+    class Config:
+        from_attributes = True
 
+class password_out(BaseModel):
+    id: int
+    platform: str
+    website_URL: str
+    platform_username: str
+    owner: UserOut
+    class Config:
+        from_attributes = True
 class TokenData(BaseModel):
     id: Optional[int] = None
 
