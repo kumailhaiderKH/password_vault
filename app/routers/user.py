4adbec2597173b95ff8fd9578ea5ab670f9a8d9c
Kumail <kumailhaider.kh@gmail.com>
2026-05-10 15:23:40 +0530

    User duplicacy handeled

--- app/routers/user.py
+++ app/routers/user.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from .. import models, schemas, utils
 from sqlalchemy.orm import Session
 from ..database import engine, SessionLocal,get_db
+from sqlalchemy.exc import IntegrityError
 
 router = APIRouter(
     prefix= '/users'
@@ -13,14 +14,17 @@ def createuser(user: schemas.UserCreate, db:Session = Depends(get_db)):
     user.password = hashed_password
     new_user = models.User(**user.dict())
     db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+    try:
+        db.commit()
+        db.refresh(new_user)
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail = f"The user {user.email} already exists")
     return new_user
 
 @router.get("/", response_model=list[schemas.UserOut])
 def get_users(db:Session = Depends(get_db)):
     users = db.query(models.User).all()
     return users
-
 
 
