0322fc2f81075030c5336cd80b7c28c8a5e4ca54
Kumail <kumailhaider.kh@gmail.com>
2026-05-10 15:36:07 +0530

    Get user

--- app/routers/user.py
+++ app/routers/user.py
@@ -27,4 +27,10 @@ def get_users(db:Session = Depends(get_db)):
     users = db.query(models.User).all()
     return users
 
+@router.get("/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if user == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"The user {id} does not exists")
+    return user
 
