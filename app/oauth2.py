8f95ec5d3479b906f5a581ee17a5be3b465bd57e
Kumail <kumailhaider.kh@gmail.com>
2026-05-30 12:21:14 +0530

    move hardcoded secrets to config settings

--- app/oauth2.py
+++ app/oauth2.py
@@ -8,16 +8,12 @@ from . import schemas, database, models, config
 oauth_scheme = OAuth2PasswordBearer(tokenUrl = 'login')
 
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes = config.settings.access_token_expire_minutes)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, config.settings.secret_key, algorithm=config.settings.algorithm)
     return encoded_jwt
 
 
@@ -25,7 +21,7 @@ def create_access_token(data: dict):
 def verify_access_token(token: str, credentials_exception):
     
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms = [ALGORITHM])
+        payload = jwt.decode(token, config.settings.secret_key, algorithms = [config.settings.algorithm])
         id: int = payload.get("user_id")
 
         if id == None:
@@ -50,11 +46,11 @@ def create_verification_token(email:str):
         "exp": expire,
         "token_type": "email_verify"
     }
-    return jwt.encode(data, SECRET_KEY, algorithm = ALGORITHM)
+    return jwt.encode(data, config.settings.secret_key, algorithm=config.settings.algorithm)
 
 def verify_verification_token(token: str):
     try:
-        payload  = jwt.decode(token, SECRET_KEY, algorithms = [ALGORITHM])
+        payload  = jwt.decode(token, config.settings.secret_key, algorithms = [config.settings.algorithm])
         email = payload.get("email")
         token_type = payload.get("token_type")
 
